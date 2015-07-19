44d61e3bb2b12965863f19c7747ba8201ba16c2e
Andy Schoenherr <jessemorganmagic@yahoo.com>
2015-07-19 13:39:56 -0400

    new files, layout, caledar

--- teetimesM/teetimesM.js
+++ teetimesM/teetimesM.js
@@ -1,19 +1,10 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
+  
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+  Template.tempName.onRendered(function() {
+    this.$('.datetimepicker').datetimepicker();
+});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
 if (Meteor.isServer) {
