d7bb4a6126ad0cdd3c0c9072dfe72dab41f20e25
Andy Schoenherr <jessemorganmagic@yahoo.com>
2015-07-31 14:34:41 -0400

    working with time picker

--- teetimesM/teetimesM.js
+++ teetimesM/teetimesM.js
@@ -1,8 +1,8 @@
 var Times = ["8:00am", "8:30am", "9:00am", "9:30am", "10:30am", "11:00am", "11:30am", "12:00pm",
  "12:30pm", "1:00pm", "1:30pm", "2:00pm", "2:30pm", "3:00pm", "3:30pm", "4:00pm", "4:30pm"];
 
- var holes9 = "$17.00";
- var holes18 = "$25.00";
+ var holes9 = 17.00;
+ var holes18 = 25.00;
 
  
 TeeTimes = new Mongo.Collection('times');
@@ -28,7 +28,12 @@ if (Meteor.isClient) {
 
   });
   
+function dateSelection(){
+var selectedDate = $("#datetimepicker").data("datetimepicker").getDate();
+console.log(dateSelection);
+};
 
+dateSelection();
 
 }
 
